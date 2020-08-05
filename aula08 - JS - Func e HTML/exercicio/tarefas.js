d10eefa0f9bfe0cc2272907a3c2b280b6679a705
ichbindionisio <ichbindionisio@edu.unisinos.br>
2020-08-05 20:05:10 -0300

    Aula 08 - Funções

---
+++ aula08 - JS - Func e HTML/exercicio/tarefas.js
@@ -0,0 +1,9 @@
+function parOuImpar(numero) {
+    if (numero % 2 == 0) {
+        console.log("O número " + numero + " é par");
+    }
+    else console.log("O número " + numero + " é impar");
+}
+
+parOuImpar(15);
+
